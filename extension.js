ef24ec4fd95b066171d63b013a5e14a5fd8514e9
trakan tantee <87230169+AJtrakan@users.noreply.github.com>
2023-10-30 01:19:36 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
     description: "Pulse Sensor or heart-rate sensor",
     author: "AJ.TAY",
     category: "Sensors",
-    version: "2.0.0",
+    version: "2.1.0",
     icon: "/static/icon.png", // Category icon
     color: "#0f3058", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
